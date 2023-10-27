642a88f79b960fd2f458c5eecbd3ba3941433e7d
Kuba <jacob.klin@buddy.works>
2023-10-27 15:16:52 +0200

    about sub

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,10 +1,77 @@
+import { useEffect } from "react";
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 import Link from 'next/link';
+import '../i18n.js';
+import { useTranslation, Trans } from 'react-i18next';
 
 export default function Home() {
+  const { t, i18n } = useTranslation();
+  const lngs = {
+    en: { nativeName: 'English' },
+    de: { nativeName: 'Deutsch' }
+  };
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return (
+
     <div className={styles.container}>
+      <header className="p-3 bg-dark text-white">
+        <div className="container">
+          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+              logo
+            </a>
+
+            <ul className="nav col-12 text-end col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
+              <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
+              <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
+              <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
+              <li><a href="#" className="nav-link px-2 text-white">About</a></li>
+            </ul>
+
+            <div className="text-end">
+              <button type="button" className="btn btn-outline-light me-2">Login</button>
+              <button type="button" className="btn btn-warning">Sign-up</button>
+            </div>
+          </div>
+        </div>
+      </header>
+      <nav>
+        <Link href="/about/about">
+          <Trans i18nKey="txt.part1"/>
+        </Link>
+      </nav>
+
+      <div className="dropdown m-3">
+        <button
+          className="btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          id="dropdownMenuButton1"
+          aria-expanded="false"
+        >
+          {i18n.resolvedLanguage}
+        </button>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          {Object.keys(lngs).map((lng) => (
+            <li key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+              {lngs[lng].nativeName}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <section>
+        <div>
+
+        </div>
+      </section>
+
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -12,7 +79,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Read <Link href="/about/about">this page!</Link>
+          Read <Link href="/about/about">{t('txt.part2')}</Link>
         </h1>
 
         <p className={styles.description}>
@@ -57,7 +124,6 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
 
